6b638fe13733173b168b13b8db59aee4008b08c2
maxpt95 <m.perez.tambasco@gmail.com>
2026-02-21 17:57:00 +0100

    Add textnode

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode, TextType
+
+
+def main():
+    print(TextNode("Testing TextNode!", TextType.TEXT, "https://www.maxtasis.com"))
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/textnode.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+from enum import StrEnum
+from typing import Any
+
+
+class TextType(StrEnum):
+    """Sum Type for inline text elements."""
+
+    TEXT = "TEXT"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINKS = "links"
+    IMAGES = "images"
+
+
+class TextNode:
+    def __init__(self, text: str, text_type: TextType, url: str | None = None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other: Any) -> bool:
+        return (
+            self.text == other.text
+            and self.text_type == other.text_type
+            and self.url == other.url
+        )
+
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
