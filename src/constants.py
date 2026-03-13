fedfcfc9b86d7baf04cd4812f273a513142bffb1
maxpt95 <m.perez.tambasco@gmail.com>
2026-03-13 09:48:27 +0100

    change public dir to docs, as github pages serve sits from the docs dir

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,4 @@
-PUBLIC_DIR_PATH = "public"
+DOCS_DIR_PATH = "docs"
 STATIC_DIR_PATH = "static"
 CONTENT_PATH = "content"
 TEMPLATE_PATH = "template.html"
--- src/main.py
+++ src/main.py
@@ -2,23 +2,23 @@ import os
 import shutil
 import sys
 
-from src.constants import CONTENT_PATH, PUBLIC_DIR_PATH, STATIC_DIR_PATH, TEMPLATE_PATH
+from src.constants import CONTENT_PATH, DOCS_DIR_PATH, STATIC_DIR_PATH, TEMPLATE_PATH
 from src.copy_static import copy_dir_recursive
 from src.generate_site import generate_pages_recursive
 
 
 def main():
-    basepath = sys.argv[0] if len(sys.argv) > 0 else "/"
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
 
-    print("Deleting public directory...")
-    if os.path.exists(PUBLIC_DIR_PATH):
-        shutil.rmtree(PUBLIC_DIR_PATH)
+    print("Deleting docs directory...")
+    if os.path.exists(DOCS_DIR_PATH):
+        shutil.rmtree(DOCS_DIR_PATH)
 
-    print("Copy static dir contents into public...")
-    copy_dir_recursive(STATIC_DIR_PATH, PUBLIC_DIR_PATH)
+    print("Copy static dir contents into docs...")
+    copy_dir_recursive(STATIC_DIR_PATH, DOCS_DIR_PATH)
 
     print("Generating site...")
-    generate_pages_recursive(basepath, CONTENT_PATH, TEMPLATE_PATH, PUBLIC_DIR_PATH)
+    generate_pages_recursive(basepath, CONTENT_PATH, TEMPLATE_PATH, DOCS_DIR_PATH)
 
 
 if __name__ == "__main__":
